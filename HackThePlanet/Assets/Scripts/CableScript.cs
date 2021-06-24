fe66714222fd63f4cd7c5150ee216ed7f56749cb
Logan Ryan <loganryan982@gmail.com>
2021-06-25 09:40:20 +1000

    Fixed cable jack movement

--- HackThePlanet/Assets/Scripts/CableScript.cs
+++ HackThePlanet/Assets/Scripts/CableScript.cs
@@ -8,6 +8,7 @@ public class CableScript : MonoBehaviour
     [SerializeField]
     float mouseZPosition = 10.0f;
 
+    public GameObject fixedCableJack;
     public GameObject moveableCablePart;
     public GameObject cablePort;
     
@@ -20,18 +21,21 @@ public class CableScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 startPosition = new Vector3(fixedCableJack.transform.position.x * 10, fixedCableJack.transform.position.y * 10, fixedCableJack.transform.position.z * 10);
+        startPosition -= new Vector3(0, 0, 1);
+
+        spline.nodes[0].Position = startPosition;
+        spline.nodes[0].Direction = startPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        spline.nodes[1].Position = moveableCablePart.transform.position;
-        spline.nodes[1].Position -= new Vector3(0, 0, 1);
-        spline.nodes[1].Direction = moveableCablePart.transform.position;
-        spline.nodes[1].Direction -= new Vector3(0, 0, 1);
-        spline.nodes[0].Direction = moveableCablePart.transform.position;
-        spline.nodes[0].Direction -= new Vector3(0, 0, 1);
+        Vector3 movePosition = new Vector3(moveableCablePart.transform.position.x * 10, moveableCablePart.transform.position.y * 10, moveableCablePart.transform.position.z * 10);
+        movePosition -= new Vector3(0, 0, 1);
+
+        spline.nodes[1].Position = movePosition;
+        spline.nodes[1].Direction = movePosition;
 
         var v3 = Input.mousePosition;
         v3.z = mouseZPosition;
@@ -42,9 +46,9 @@ public class CableScript : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            Physics.Raycast(ray, out hit);
+            bool rayHit = Physics.Raycast(ray, out hit);
 
-            if (hit.collider.gameObject.name == moveableCablePart.name)
+            if (rayHit && hit.collider.gameObject.name == moveableCablePart.name)
             {
                 moveableCablePart.transform.position = v3;
             }
