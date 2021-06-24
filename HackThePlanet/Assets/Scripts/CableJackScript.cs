1b453d619bc17f34ba90a2d575a1baab24bb4cff
Logan Ryan <loganryan982@gmail.com>
2021-06-24 16:53:29 +1000

    Created cable script for servers

---
+++ HackThePlanet/Assets/Scripts/CableJackScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableJackScript : MonoBehaviour
+{
+    CableScript cableScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cableScript = GetComponentInParent<CableScript>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    { 
+        if (other.gameObject.name == cableScript.cablePort.name)
+        {
+            cableScript.interactable = false;
+
+            cableScript.moveableCablePart.transform.position = cableScript.cablePort.transform.position;
+        }
+    }
+}
