ea4ad081fcc0421275b2eff5bed0a903fde047e5
Mara Dusevic <s200494@students.aie.edu.au>
2021-06-24 15:34:22 +1000

    Added node connections

--- HackThePlanet/Assets/Scripts/HackingNode.cs
+++ HackThePlanet/Assets/Scripts/HackingNode.cs
@@ -2,17 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class HackingNode : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Fields
+
+    //[HideInInspector]
+    public Vector3 m_position;
+
+    [SerializeField]
+    public float m_timeValue = 0;
+
+    [SerializeField]
+    public bool m_isStartNode = false;
+
+    [SerializeField]
+    public bool m_isEndNode = false;
+
+    [SerializeField]
+    public HackingNode[] m_connections;
+
+    #endregion
+
+    #region Constructors
+
+    public HackingNode()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public HackingNode(Vector3 a_position, float a_timeValue, bool a_isStartNode, bool a_isEndNode)
     {
-        
+        m_position = a_position;
+        m_timeValue = a_timeValue;
+        m_isStartNode = a_isStartNode;
+        m_isEndNode = a_isEndNode;
+    }
+
+    public HackingNode(Vector3 a_position, float a_timeValue, bool a_isStartNode, bool a_isEndNode, HackingNode[] a_connections)
+    {
+        m_position = a_position;
+        m_timeValue = a_timeValue;
+        m_isStartNode = a_isStartNode;
+        m_isEndNode = a_isEndNode;
+        m_connections = a_connections;
+    }
+
+    #endregion
+
+    public void Start()
+    {
+        m_position = gameObject.transform.position;
+    }
+
+    public void Update()
+    {
+        //m_position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
     }
 }
---
+++ HackThePlanet/Assets/Scripts/LineConnection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineConnection : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    private bool hasDrawn = false;
+
+    private void Update()
+    {
+        if (!hasDrawn)
+        {
+            DrawConnections();
+            hasDrawn = true;
+        }
+    }
+
+    public void DrawLine(Vector3 a_node1, Vector3 a_node2, RectTransform a_line)
+    {
+        a_line.transform.position = a_node1;
+
+        float distance = Vector3.Distance(a_node1, a_node2) + 10;
+
+        a_line.transform.position = (a_node1 + a_node2) / 2;
+        a_line.rect.Set((a_node1.x + a_node2.x) / 2, (a_node1.y + a_node2.y) / 2, distance, 0.005f);
+
+        Vector3 dir = a_node2 - a_node1;
+        Vector2 dirV2 = new Vector2(dir.x, dir.y);
+        float angle = Vector2.SignedAngle(dirV2, Vector2.down);
+
+        a_line.transform.rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+    }
+
+    public void DrawConnections()
+    {
+        HackingNode[] allNodes = FindObjectsOfType<HackingNode>();
+
+        if (allNodes.Length == 0)
+        {
+            return;
+        }
+
+        foreach (HackingNode node in allNodes)
+        {
+            if (node.m_connections.Length != 0)
+            {
+                foreach (HackingNode connection in node.m_connections)
+                {
+                    GameObject line = Instantiate(linePrefab, node.gameObject.transform.parent);
+                    line.GetComponent<LineDrawer>().DrawLine(line, node.m_position, connection.m_position);
+
+                    //GameObject line = Instantiate(linePrefab, node.gameObject.transform.parent);
+                    //DrawLine(node.m_position, connection.m_position, line.GetComponent<RectTransform>());
+                }
+            }
+        }
+    }
+}
