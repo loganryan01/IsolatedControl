6233d77da02ec033d187cc9e1396a9cc3e94b146
Logan Ryan <loganryan982@gmail.com>
2021-06-24 12:14:36 +1000

    Created script to control power box puzzle

---
+++ HackThePlanet/Assets/Scripts/PowerBoxScript.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBoxScript : MonoBehaviour
+{
+    [SerializeField]
+    int overallNumber;
+    int currentNumber;
+
+    [SerializeField]
+    Text overallNumberText;
+
+    [SerializeField]
+    int[] switchValues;
+
+    [SerializeField]
+    GameObject[] switches;
+    
+    [SerializeField]
+    Text[] switchValueTexts;
+    
+    Animator animator;
+
+    bool resetting = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        overallNumberText.text = overallNumber.ToString();
+        
+        for (int i = 0; i < switchValueTexts.Length; i++)
+        {
+            switchValueTexts[i].text = switchValues[i].ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Physics.Raycast(ray, out hit);
+
+            if (hit.collider.gameObject.CompareTag("Switch"))
+            {
+                animator = hit.collider.gameObject.GetComponent<Animator>();
+
+                if (!animator.GetBool("Activated"))
+                {
+                    animator.SetBool("Activated", true);
+                    
+                    // Check what switch has been touched
+                    for (int i = 0; i < switches.Length; i++)
+                    {
+                        if (hit.collider.gameObject.name == switches[i].name)
+                        {
+                            currentNumber += switchValues[i];
+                        }
+                    }
+                }
+            }
+        }
+
+        // If no animations are playing and the player's current number is greater than the overall number
+        if (!resetting && !AnimationIsPlaying() && currentNumber > overallNumber)
+        {
+            StartCoroutine(ResetSwitches());
+        }
+    }
+
+    // Check if an animation is playing
+    bool AnimationIsPlaying()
+    {
+        Animator[] switchAnimators = new Animator[5];
+
+        for (int i = 0; i < switchAnimators.Length; i++)
+        {
+            switchAnimators[i] = switches[i].GetComponent<Animator>();
+
+            if (switchAnimators[i].GetCurrentAnimatorStateInfo(0).normalizedTime < 1 &&
+                switchAnimators[i].GetCurrentAnimatorStateInfo(0).IsName("Switch"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Return the switches to their original positions
+    IEnumerator ResetSwitches()
+    {
+        resetting = true;
+        
+        Animator[] switchAnimators = new Animator[5];
+
+        yield return new WaitForSeconds(1.0f);
+
+        for (int i = 0; i < switchAnimators.Length; i++)
+        {
+            switchAnimators[i] = switches[i].GetComponent<Animator>();
+            switchAnimators[i].SetBool("Activated", false);
+        }
+
+        currentNumber = 0;
+
+        resetting = false;
+    }
+}
