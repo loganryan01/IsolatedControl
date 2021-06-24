207f92c3aefdfa4fbcd2638e146b329494bcd4f9
Andrew Phillip Gonzalez <Lekonia@iinet.net.au>
2021-06-24 18:06:39 +1000

    Add Faux/Psuedo police lights
    added police lights, needs tweaking as light intensity is not enough

---
+++ HackThePlanet/Assets/Scripts/Light Scripts/SirenLights.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SirenLights : MonoBehaviour
+{
+    [SerializeField] Light redLight;
+    [SerializeField] Light blueLight;
+
+    private Vector3 redTemp;
+    private Vector3 blueTemp;
+
+    [SerializeField] int speed;
+
+    void Update()
+    {
+        redTemp.y += speed * Time.deltaTime;
+        blueTemp.y -= speed * Time.deltaTime;
+
+        redLight.transform.eulerAngles = redTemp;
+        blueLight.transform.eulerAngles = blueTemp;
+    }
+}
