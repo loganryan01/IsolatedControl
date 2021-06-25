3ac08f4e162f6d2af65f0256d129211d8b433cec
Logan Ryan <loganryan982@gmail.com>
2021-06-25 10:58:38 +1000

    Created main menu screen

---
+++ HackThePlanet/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject fade;
+    public Animator fadeEffect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeEffect()
+    {
+        Debug.Log("Fading");
+
+        fade.SetActive(true);
+        fadeEffect.enabled = true;
+    }
+}
