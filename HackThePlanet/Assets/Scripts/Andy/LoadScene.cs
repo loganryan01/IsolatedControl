614540e84ad6fc7bdaea906aca53da262d5a6df4
Andrew Phillip Gonzalez <Lekonia@iinet.net.au>
2021-06-27 15:41:02 +1000

    Organise
    Fixed a scene transition issue with the intro story to the main game, something to do with... code stuff, organised some folders... it has been a long day

---
+++ HackThePlanet/Assets/Scripts/Andy/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    //public Object gameScene;
+
+    void Start()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
